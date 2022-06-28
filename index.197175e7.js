d94cc5957edcfe7fe561ff82a717ce814a883037
ilshmelev <93441848+ilshmelev@users.noreply.github.com>
2022-06-28 13:33:54 +0000

    Deploying to gh-pages from @ ilshmelev/project-12@bd9b74f9b092c2619c5bdeb4ac7877139bb9c74d 🚀

---
+++ index.197175e7.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),closeMenuBtn:document.querySelector("[data-menu-close]"),menu:document.querySelector("[data-menu]")};function n(){e.menu.classList.toggle("is-open"),document.body.classList.toggle("menu-open")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.197175e7.js.map
